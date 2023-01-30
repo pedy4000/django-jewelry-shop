6af9168101978ab52f788ec31e6ac9892a0f9d09
pedram <pedy4000@gmail.com>
2023-01-30 18:47:06 +0330

    Update env.groovy

--- env.groovy
+++ env.groovy
@@ -1,6 +1,6 @@
 SERVICE_NAME="django-jewelry-shop"
-SERVICE_BRANCH_NAME="origin dev"
-SERVICE_GIT_URL="https://github.com/pedy4000/django-jewelry-shop"
+SERVICE_BRANCH_NAME="origin main"
+SERVICE_GIT_URL="hhttps://github.com/pedy4000/deploy-script"
 PORT=24900
 USER="dockeru"
 IP="127.0.0.1"
